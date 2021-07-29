e733557f085b6081f45916a2a595506880c33088
Gopi Balaraman <gobi.b.trueconnect@swymcorp.com>
2021-07-29 20:43:49 +0530

    changes for dublicate list

--- src/components/WishlistModel/index.js
+++ src/components/WishlistModel/index.js
@@ -1,15 +1,107 @@
 import React, { useState, useEffect } from "react";
 import {Form, Modal, Button, Card } from "react-bootstrap";
 import "./style.css";
+import * as _ from "lodash";
+import { useDispatch } from 'react-redux';
+import { fetchWishlistCateogory } from "../../app/reducer/wishlist-reducer"
+
+const Counter = (props) => {
+    let { count, setCount } = props;
+    const [quantity, setQuantity] = useState(0);
+    
+    useEffect(()=>{
+        setQuantity(count?count:0);
+    },[count]);
+
+    return (
+        <div className="row">
+            <div className="col-6">
+                <div>Quantity</div>
+                <div>{quantity}</div>
+            </div>
+            <div className="col-6">
+                <div>
+                    <Button className="m-1" onClick={()=>{
+                        setQuantity(quantity+1);
+                        setCount(quantity+1);
+                    }}>+</Button>
+                </div>
+                <div>
+                    <Button className="m-1" onClick={()=>{
+                        if(quantity>0){
+                            setQuantity(quantity-1);
+                            setCount(quantity-1);
+                        }
+                    }}>-</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const TotalDetails = (props) => {
+    let { selectedList } = props;
+    
+    const [totalQuantity, setTotalQuantity] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    useEffect(()=>{
+        console.log('on calculate total quantity');
+        let totalQuantity = 0;
+        let price = 0;
+        selectedList.map(item=>{
+            totalQuantity += item.qty?item.qty:0;
+            price += (item.pr*(item.qty?item.qty:0));
+        });
+        setTotalQuantity(totalQuantity);
+        setTotalPrice(price);
+    })
+
+    return (
+        <div className="row">
+            <div>Total Quantity {totalQuantity}</div>
+            <div>Total Price {totalPrice}</div>
+        </div>
+    )
+}
 
 const WishlistModel = (props) => {
     
     let { open, hide, productList } = props;
     console.log('on open ', productList);
+    const dispatch = useDispatch();
+    const [ allProducts, setAllProducts ] = useState([]);
     const [ selected, setSelected ] = useState([]);
+    const [showAddList, setShowAddList] = useState(false);
+    const [newListName, setNewListName] = useState('');
+
+    const createNewWishlist = ( ) => {
+        window._swat.createList({lname: newListName}, ({lid})=>{
+            // successfully created list
+            console.log("New list created with listid", {lid, selected});
+            setNewListName(null);
+            dispatch(fetchWishlistCateogory());
+            let newList = selected.map((item)=>{
+                let { epi, empi, du, qty, note, cprops } = item;
+                return { epi, empi, du, qty, note, cprops };
+            })
+            window._swat.addProductsToList(lid, newList, (newListItem)=>{
+                // successfully added list item
+                console.log('on new list added',newListItem);
+                dispatch(fetchWishlistCateogory());
+              }, (xhrObj) => {
+                // something went wrong
+                console.log('error add products list',xhrObj);
+              });
+          }, (xhrObject)=>{
+            // something went wrong
+            console.log('error = ',xhrObject);
+          });
+    }
 
     useEffect(()=>{
-        setSelected(productList);
+        setSelected(_.cloneDeep(productList));
+        setAllProducts(_.cloneDeep(productList));
     },[productList.length])
     
     return (
@@ -18,19 +110,31 @@ const WishlistModel = (props) => {
                 <Modal.Title>Make a draft order</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div>Products</div>
+                <div className="d-flex">
+                    <Form.Check type="checkbox" label="" checked={(selected && selected.length == productList.length)} 
+                        onChange={(checkedItem)=>{
+                            console.log('on checked change', checkedItem.target.checked);
+                            if(checkedItem.target.checked){
+                                setSelected(productList);
+                            }else{
+                                setSelected([]);
+                            }
+                        }} 
+                    className="m-2 p-2" />    
+                    <span className="p-2 h5 my-auto">{selected.length} Products Selected</span>
+                </div>
+
                 <div>
-                    { productList && productList.map((item, index)=>{
+                    { allProducts && allProducts.map((item, index)=>{
+                        console.log('product list changed', allProducts);
                         let selectedList = [...selected];
                         let checkedIndex = selected && selected.findIndex(selectedItem=>selectedItem.id==item.id);
-
                         return (
-                            <Card key={index} className="row m-2 p-3" as="a" href={item.du} style={{ textDecoration: 'none' }} >
+                            <Card key={index} className="row m-2 p-3" style={{ textDecoration: 'none' }} >
                                 <div className="row">
                                     <div  className="col-sm-12 col-md-3 col-lg-3 p-2 d-flex" >
                                         <Form.Check type="checkbox" label="" checked={(checkedIndex>-1)} onChange={(checkedItem)=>{
-                                            console.log('on checked change', checkedItem.target.checked);
-                                            if(checkedItem.target.checked){
+                                            console.log('on checked change', checkedItem.target.checked);                    if(checkedItem.target.checked){
                                                 selectedList.push(item);
                                                 setSelected(selectedList);
                                             }else{
@@ -41,14 +145,55 @@ const WishlistModel = (props) => {
                                         <img alt="" height="100" src={item.iu} />
                                     </div>
                                     <div className="col-sm-12 col-md-6 col-lg-8 p-2">
-                                        <h4>{item.dt}</h4>
-                                        <h5>${item.pr}</h5>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <h4>{item.dt}</h4>
+                                                <h5>${item.pr}</h5>
+                                            </div>
+                                            <div className="col-6 row">
+                                                <Counter count={item.qty} setCount={(count)=>{
+                                                    item.qty = count;
+                                                    selectedList[checkedIndex].qty = count;
+                                                    setSelected([...selected]);
+                                                }} />
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </Card>
 					    )
                     })}
                 </div>
+                <div>
+                    <TotalDetails selectedList={selected} />
+                </div>
+                <Modal size="lg" show={showAddList} onHide={()=>setShowAddList(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create New List</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Wishlist new name</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                value={newListName}
+                                onChange={(event)=>{
+                                    setNewListName(event.target.value);
+                                }}
+                                placeholder="name" />
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" disabled={selected.length==0} onClick={()=>{
+                            console.log('on Submit, Selected Products',selected);
+                            setShowAddList(false);
+                            hide();
+                            createNewWishlist();
+                        }}>
+                            Create List
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={hide}>
@@ -57,7 +202,17 @@ const WishlistModel = (props) => {
                 <Button variant="primary" disabled={selected.length==0} onClick={()=>{
                     console.log('on Submit, Selected Products',selected);
                 }}>
-                    Submit
+                    Creat Cart
+                </Button>
+                <Button 
+                    variant="primary" 
+                    disabled={selected.length==0} 
+                    onClick={()=>{
+                        console.log('on Submit, Selected Products',selected);
+                        setShowAddList(true);
+                    }}
+                >
+                    Dublicate List
                 </Button>
             </Modal.Footer>
         </Modal>
--- src/components/wishlist/index.js
+++ src/components/wishlist/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchWishlistCateogory, fetchWishList } from "../../app/reducer/wishlist-reducer"
 import WishlistModel from '../WishlistModel';
@@ -18,24 +18,49 @@ const Wishlist = () => {
 		dispatch(fetchWishlistCateogory());
 	}, []);
 
+	let productList = _.cloneDeep(wishlist);
+
 	console.log('wishlist ', wishlist);
 	return (
 		<div className="p-5">
 			<div className="row">
 				{
 					wishlistCategory && wishlistCategory.map((item, index)=>(
-						<Card key={index} className="p-4 mt-2" onClick={()=>{
-							//setSelectedList(item.listcontents);
-							console.log('on item', item);
-							dispatch(fetchWishList({lid:item.lid}));
-							setShowWishlist(true);
-						}}>
-							<h3>{item.lname}</h3>
+						<Card key={index} className="p-4 mt-2" 
+						>
+							<div className="row">
+								<div className="col-10">
+									<h3>{item.lname}</h3>
+								</div>
+								<div className="col-2">
+									<Button 
+										variant="danger"
+										onClick={(event)=>{
+										event.preventDefault();
+										window._swat.deleteList(item.lid, (deletedListObj)=>{
+											// successfully deleted list
+											console.log("Deleted list with listid", deletedListObj.lid);
+											dispatch(fetchWishlistCateogory());
+										  }, (xhrObject)=>{
+											// something went wrong
+										  });
+									}}>Delete</Button>
+									<Button 
+										className="m-1"
+										onClick={()=>{
+											//setSelectedList(item.listcontents);
+											console.log('on item', item);
+											dispatch(fetchWishList({lid:item.lid}));
+											setShowWishlist(true);
+										}}
+									>View</Button>
+								</div>
+							</div>
 						</Card>
 					))
 				}
 			</div>
-			<WishlistModel open={showWishlist} hide={()=>setShowWishlist(false)} productList={wishlist} />
+			<WishlistModel open={showWishlist} hide={()=>setShowWishlist(false)} productList={productList} />
 		</div>
 	);
 }
